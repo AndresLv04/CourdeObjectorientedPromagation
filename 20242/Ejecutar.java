326a7ed396acb16ea338937d8927e49974719692
AndresLv04 <166462520+AndresLv04@users.noreply.github.com>
2024-08-12 13:08:15 +0000

    comentario

---
+++ 20242/Ejecutar.java
@@ -0,0 +1,15 @@
+public class Ejecutar{
+    public static void main(String[] args) {
+        
+        //Comentario para una sola linea
+        
+        /*
+         * Comentario
+         * para
+         * varias lineas
+         */
+
+         //JAVA la asignación 
+        
+    };
+};
