ce9f3771251a50bac3b03adccd07b2115904be0a
AndresLv04 <166462520+AndresLv04@users.noreply.github.com>
2024-08-17 17:10:50 +0000

    Realizando ejercicio 3

--- Taller 1/JavaScript/javaScript.js
+++ Taller 1/JavaScript/javaScript.js
@@ -3,3 +3,10 @@ function areaCuadrado(){
     let areaTotal = lado * lado;
     document.getElementById("input_resultado1").innerHTML = areaTotal;
 }
+
+function areaCirculo(){
+    const PI = 3.1416;
+    let radio = document.getElementById("input_ejercicio2").value;
+    let areaTotal = PI*Math.pow(radio,2);
+    document.getElementById("input_resultado2").innerHTML = areaTotal;
+}
