f1adf259ef3357b4d2142ca1974ae75b9b19c2b5
AndresLv04 <166462520+AndresLv04@users.noreply.github.com>
2024-08-17 18:50:25 +0000

    Terminado ejercicio 3

--- Taller 1/JavaScript/javaScript.js
+++ Taller 1/JavaScript/javaScript.js
@@ -10,3 +10,18 @@ function areaCirculo(){
     let areaTotal = PI*Math.pow(radio,2);
     document.getElementById("input_resultado2").innerHTML = areaTotal;
 }
+
+function grados(){
+
+    if (document.getElementById("tipo_option").value == "CaF") {
+        let gC = document.getElementById("input_ejercicio3").value;
+        let convertirG = ((9/5) * gC) + 32;
+        document.getElementById("input_resultado3").innerHTML = convertirG + " grados °F";
+    }if (document.getElementById("tipo_option").value == "FaC") {
+        let gF = document.getElementById("input_ejercicio3").value;
+        let convertirF = (gF - 32)/(9/5);
+        document.getElementById("input_resultado3").innerHTML = convertirF.toFixed(3) + " grados °C"
+    } else {
+        
+    }
+}
